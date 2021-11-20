4bafce06d91e45ae77058d59a117a4d2ca0f4ba7
thallyssonklein <thallyssonklein@gmail.com>
2021-11-20 12:44:53 -0300

    feat: open hackerrank algorithm problem on a new tab

---
+++ README.md
@@ -0,0 +1,11 @@
+# Random Hackerrank
+
+Just enter https://www.hackerrank.com/domains/algorithms and click on the extension icon and the same will open a random coding challenge on a new tab. 
+
+**Ps:** Please search how to load an unpacked extension on chrome before try to run this and for run:
+
+```
+yarn;yarn dev
+```
+
+**Attention:** Load the entire project's folder instead of only the dist folder
--- src/background.js
+++ src/background.js
@@ -1,5 +1,27 @@
 console.log("Background script")
 
 chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.sendMessage(tab.id, "screenshot")
+    chrome.tabs.query(
+        { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+        function(tabs) {
+          const { id: tabId } = tabs[0].url;
+      
+          let code = `
+            (
+                function() {
+                    let result = document.querySelector('.challenges-list');
+
+                    let item = result.children[Math.floor(Math.random()*result.children.length)];
+                    
+                    return item.href;
+                }
+            )();
+         `;
+      
+          chrome.tabs.executeScript(tabId, { code }, function (result) {
+            chrome.tabs.create({'url': result[0]});                         
+          });
+        }
+      );
+      
 }) 
